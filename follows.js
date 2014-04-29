7d1265ad488370f832b6317f76aabf0d86073178
ralphespanhol <ralphespanhol@users.noreply.github.com>
2014-04-28 23:23:12 -0300

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= 'JbOrawDX5LIMsTOmmqjfhuEJX5JM0Xfk7fkD6t6LrKpS2JtX', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1022109852], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'SENSOR DATA', // Replaces Xively logo in the header
+		applicationName	= 'Environmental Monitoring', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '1B445D', // CSS HEX value of color to represent data (omit leading #)
